8a1d1bd899728b2aafae3caf0c704819962b66c9
stephen498 <taiwostephen73@gmail.com>
2022-08-03 10:43:18 +0100

    commit

--- app.js
+++ app.js
@@ -8,28 +8,40 @@ function startApp() {
     const scrabble = document.getElementById('scrabble');
     const sym = document.getElementById('symbol');
     const submit = document.getElementById('submit');
-    let count = 0;
+    let count = [];
+
+    let count1 = 0;
     let counter = 0;
     let symbols = [',', '.', '_', ':', ';', '?', '@'];
+    let scrab = scrabble.value.split(',');
+    for (let i = 0; i < scrab.length; i++) {
+        count[i] = 0;
+    }
     for (let key of words) {
-        if (key.slice(0, scrabble.value.length).toLowerCase() == (scrabble.value).toLowerCase()) {
-            count += 1;
+        for (let i = 0; i < scrab.length; i++) {
+            if (key.slice(0, scrab[i].length).toLowerCase() == (scrab[i]).toLowerCase()) {
+                count[i] += 1;
+            }
         }
         if (!symbols.includes(key)) {
             counter += 1;
         }
     }
+    console.log(scrab);
     let redact = words.map((item, index, array) =>
-        (item.slice(0, scrabble.value.length).toLowerCase() != (scrabble.value).toLowerCase()) ? item :
+
+        (item.slice(0, scrab[0].length).toLowerCase() != scrab[0].toLowerCase()) && (item.slice(0, scrab[1].length).toLowerCase() != scrab[1].toLowerCase()) && (item.slice(0, scrab[2].length).toLowerCase() != scrab[2].toLowerCase()) ? item :
 
         // for (let i = 1; i < (item.length - 1); i++) {
         //     item[i] = sym;
         // }
+
         item = item.split('').map((char, index, array) =>
             (index == 0 || index == array.length - 1) ? char : sym.value
         ).join('')
 
-    )
+
+    );
     console.log(redact);
     const final = redact.join(' ');
     console.log(final);
@@ -64,12 +76,20 @@ function startApp() {
     perfect.style.textAlign = 'center';
     perfect.style.marginLeft = '155px';
     const score = document.createElement('p');
-    score.innerText = ` scrabbled word  
-                        ${count}  `;
+    score.innerText = ` scrabbled word ${scrab[0]}   
+                        ${count[0]}`
     score.id = 'score';
+    const score1 = document.createElement('p');
+    score1.innerText = `scrabbled word ${scrab[1]}
+                        ${count[1]}`;
+    score1.id = 'score1';
+    const score2 = document.createElement('p');
+    score2.innerText = `scrabbled word ${scrab[2]}
+                        ${count[2]}`;
+    score2.id = 'score2';
     const number = document.createElement('p');
-    number.innerText = ` number of words 
-                          ${counter}`;
+    number.innerText = `number of words
+                        ${counter}`;
     content.remove();
     scrabble.remove();
     sym.remove();
@@ -77,12 +97,14 @@ function startApp() {
     document.body.prepend(timer);
     document.body.prepend(perfect);
     document.body.prepend(score);
+    document.body.prepend(score1);
+    document.body.prepend(score2);
     // document.getElementById('score').insertAdjacentElement('afterend', number)
     document.body.prepend(number);
     const grid = document.querySelectorAll('p');
     for (let key of grid) {
         key.style.width = '250px';
-        key.style.height = '200px';
+        key.style.height = '300px';
         key.style.border = '1px solid black';
         key.style.borderRadius = '5px';
         key.style.fontSize = '50px';
@@ -90,6 +112,6 @@ function startApp() {
         key.style.textAlign = 'center';
         // perfect.style.marginLeft = '155px';
         key.style.display = 'inline-block';
-        key.style.margin = '100px'
+        key.style.margin = '30px'
     }
 }
